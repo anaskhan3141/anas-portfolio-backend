4f749dfcd375ebe726d415708970307f511e4c12
Anas Khan <anas.raheem2003@gmail.com>
2024-02-18 17:51:21 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+require('dotenv').config()
+
+
+
+
+
+const app = express()
+app.use(cors());
+
+
+app.use(express.json())
+
+app.post('/email', (req, res, next) => {
+    console.log('reqested')
+    const { name, email, message } = req.body
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.USER_EMAIL,//'anas.raheem2002@gmail.com',
+            pass: process.env.APP_PASSWORD
+        }
+
+    })
+
+    const mailOptions = {
+        from: 'anas.raheem2002@gmail.com',
+        to: 'anas.raheem2003@gmail.com',
+        subject: 'Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.send(500,'Internal Server Error');
+        } else {
+            res.send(200,{"res":info.response,"envelope":info.envelope});
+        }
+    });
+})
+
+
+
+app.listen(process.env.PORT, () => {
+
+    console.log(`server started on ${process.env.PORT}`)
+})
+
+exports.api = functions.https.onRequest(app)
+
+
+
+
+
